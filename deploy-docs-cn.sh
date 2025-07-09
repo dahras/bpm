6e99f19f39764774273e55b5b0a8a0889840a38e
dahras <dahras@gmail.com>
2025-07-10 00:03:45 +0530

    chore: update branding to docobase

--- deploy-docs-cn.sh
+++ deploy-docs-cn.sh
@@ -1,8 +1,8 @@
 cd docs/dist/zh-CN
-echo "docs-cn.nocobase.com" >> CNAME
+echo "docs-cn.docobase.com" >> CNAME
 echo "" >> .nojekyll
 git init
-git remote add origin git@github.com:nocobase/docs-cn.nocobase.com.git
+git remote add origin git@github.com:docobase/docs-cn.docobase.com.git
 git branch -M main
 git add .
 git commit -m "first commit"
